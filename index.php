7e9fd7a49836cf9f8e0cdac4603e6bca34e84167
Laks-Minu <98518137+Laks-Minu@users.noreply.github.com>
2022-01-31 15:56:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <body> 
 	<h1>PHP 2022  Test</h1>
 		<p><b> CITIBANK Session </b></p>
-		<?php echo "The Current Date and yes las  Time is: <br />";
+		<?php echo "LAKS<br />";
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information VERSION 2 </h2> 
 		<p> <?php phpinfo(); ?> </p> 
